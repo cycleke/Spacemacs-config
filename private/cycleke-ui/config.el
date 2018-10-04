82eebc1a354e8100b73b68bd8a712e75503d75c6
cycleke <cycle@gmail.com>
2018-10-04 15:37:22 +0800

    Modify UI configuration.

--- private/cycleke-ui/config.el
+++ private/cycleke-ui/config.el
@@ -1,6 +1,11 @@
 (setq frame-title-format
       '("%S" (buffer-file-name "%f"
                                (dired-directory dired-directory "%b"))))
+
+(display-time-mode t)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+
 ;; Global settings (defaults)
 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
       doom-themes-enable-italic t) ; if nil, italics is universally disabled
