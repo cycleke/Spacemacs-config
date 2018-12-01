7dcd0506f80418cc253a71a2faeb5c5e2998bd6a
cycleke <cycleke@gmail.com>
2018-12-01 12:34:50 +0800

    modified:   init.el

--- init.el
+++ init.el
@@ -252,9 +252,9 @@
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         doom-city-lights
                          oldlace
                          organic-green
+                         doom-city-lights
                          spacemacs-dark
                          spacemacs-light)
 
